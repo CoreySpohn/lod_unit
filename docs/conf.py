ca967ffb9a6c5d523aab08ebdbc3726fafb143b3
Corey Spohn <cas584@cornell.edu>
2024-03-26 17:37:35 -0400

    fix(icon): Added necessary information to the docs config for logo and github links

--- docs/conf.py
+++ docs/conf.py
@@ -26,3 +26,14 @@ autodoc_typehints = "description"
 html_theme = "sphinx_book_theme"
 html_static_path = ["_static"]
 master_doc = "index"
+html_logo = "_static/logo.png"
+html_favicon = "_static/favicon.ico"
+html_title = "λ/D Unit and Equivalency Module"
+html_theme_options = {
+    "path_to_docs": "docs",
+    "repository_url": "https://www.github.com/CoreySpohn/lod_unit",
+    "repository_branch": "main",
+    "use_edit_page_button": True,
+    "use_repository_button": True,
+    "use_issues_button": True,
+}
