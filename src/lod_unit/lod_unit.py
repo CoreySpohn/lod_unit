baabcac54ce5d9b4ed0811078f1c0adcf47870c6
Corey Spohn <cas584@cornell.edu>
2024-03-28 09:20:29 -0400

    fix(main): Adding parentheses that were lost in translation

--- src/lod_unit/lod_unit.py
+++ src/lod_unit/lod_unit.py
@@ -33,14 +33,16 @@ def lod_eq(lam: Quantity, D: Quantity) -> Equivalency:
         >>> angseparation.to(u.arcsec, lod_eq(lam, diam))
             <Quantity 0.03093972 arcsec>
     """
+    _lam = lam.to(u.m).value
+    _D = D.to(u.m).value
 
     # Conversion functions
-    def lod_to_rad(lod_val: Quantity) -> Quantity:
-        return (lod_val * (lam / D)).decompose().value * u.rad
+    def lod_to_rad(lod_val: float) -> float:
+        return lod_val * (_lam / _D)
 
-    def rad_to_lod(rad_val: Quantity) -> u.Quantity:
-        return (rad_val * (D / lam)).decompose().value * lod
+    def rad_to_lod(rad_val: float) -> float:
+        return rad_val * (_D / _lam)
 
-    base_equivalence = [lod, u.rad, lod_to_rad, rad_to_lod]
+    base_equivalence = [(lod, u.rad, lod_to_rad, rad_to_lod)]
 
     return Equivalency(base_equivalence)
